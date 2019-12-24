54b0422817fcc14b4c50b49e6244a3e178ccf394
zhouzhongyi1 <zhouzhongyi1@jd.com>
2019-12-24 17:11:38 +0800

    jdkAop处理args

--- spring-mvc-web/src/main/java/notes/mvc/web/aop/aspect/PointCutAspect.java
+++ spring-mvc-web/src/main/java/notes/mvc/web/aop/aspect/PointCutAspect.java
@@ -21,6 +21,21 @@ public class PointCutAspect {
      *
      * 此方法会匹配入参是一个或多个形参的目标方法织入，并且第一个参数类型必须是String
      *
+     * args() 无参
+     * args(*) 一个参数
+     * args(..) 0个或任意个参数
+     *
+     * 切入点表达式：
+     * within(notes.mvc.service.aop.*)  该包下的所有方法连接点
+     * within(notes.mvc.service.aop..*) 该包及其子包的所有方法连接点
+     *
+     * this(notes.mvc.service.aop.JdkAopService)    代理对象是该类或接口实例的所有方法
+     * target(notes.mvc.service.aop.JdkAopService)  目标对象该类或接口实例的所有方法
+     *
+     * args(notes.mvc.service.aop.JdkAopService)    实参(注意不是方法定义)传入的是JdkAopService类型的方法
+     *
+     * bean(*AopService)    匹配所有id以AopService结尾的Bean里面的方法
+     *
      * @param jp
      * @param food
      */
